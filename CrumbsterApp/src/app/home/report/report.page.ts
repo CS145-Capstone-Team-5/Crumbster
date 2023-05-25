c2f837f07d7f81a61b05c31079c9299f120715be
Jeanne Toledo <jeanneclarissetoledo@gmail.com>
2023-05-25 21:01:44 +0800

    feat: Create page specifically for report

---
+++ CrumbsterApp/src/app/home/report/report.page.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { IonicBackendService } from 'src/app/ionic-backend.service';
+
+@Component({
+  selector: 'app-report',
+  templateUrl: './report.page.html',
+  styleUrls: ['./report.page.scss'],
+})
+export class ReportPage implements OnInit {
+  public factsOrTips!: string;
+  public weight!: string;
+
+  constructor(private _api: IonicBackendService) { }
+
+  ngOnInit() {
+    // Method called to obtain data from the server via HTTP GET
+    this._api.getApi()
+      .subscribe(data => {
+        this.factsOrTips = JSON.stringify(data.fact) || JSON.stringify(data.tip)
+        this.weight = JSON.stringify(data.dataLog).replace(/["]+/g, '')
+      });
+    if (this.factsOrTips === null || this.factsOrTips === undefined) {
+      this.factsOrTips = ""
+    }
+    this.factsOrTips.replace(/["]+/g, '');
+    if (this.weight === null || this.weight === undefined) {
+      this.weight = ""
+    }
+  }
+}
