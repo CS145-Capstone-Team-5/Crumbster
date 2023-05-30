ed19c45f4acc5a8180cf4c77a83093d895a442a8
aysycs140 <aysy@up.edu.ph>
2023-05-30 13:59:32 +0800

    Added more notification endpoints for actuation, separated facts and tips

---
+++ factsNtips.py
@@ -0,0 +1,13 @@
+# Sample Dictionaries storing the fun facts and tips to be displayed
+
+facts = {
+    'fact1': {'fact': "The scale of the current global hunger and malnutrition crisis is enormous, with more than 345 million people facing high levels of food insecurity in 2023 - more than double the number in 2020. - WFP"},
+    'fact2': {'fact': "More than 900,000 people worldwide are fighting to survive in catastrophic hunger/one step away from famine. This is ten times more than five years ago, an alarmingly rapid increase. - WFP"},
+    'fact3': {'fact': "The climate crisis is one of the leading causes of the steep rise in global hunger. Climate shocks destroy lives, crops and livelihoods, and undermine people's ability to feed themselves. - WFP"}
+}
+
+tips = {
+    'tip4': {'tip': "Preplan and write your shopping list before going to the grocery store. As you write your list, think about what meals you will be preparing the following week, and check your fridge to see what items you already have. - FDA"},
+    'tip5': {'tip': "When at the store, buy only what you need and stick to your shopping list. Be careful when buying in bulk, especially with items that have a limited shelf life. - FDA"},
+    'tip6': {'tip': "Prepared too much food for a party at your home? Pack extras in containers for guests to take home or take some over to a neighbor as a nice gesture. - FDA"}
+}
--- notifs.py
+++ notifs.py
@@ -9,8 +9,34 @@ default_app = firebase_admin.initialize_app(cred)
 # This registration token comes from the client FCM SDKs.
 registration_token = 'REGISTRATION-TOKEN'
 
-message = messaging.Message(
-    notification=_messaging_utils.Notification('Crumbster Alert','Time to check today\'s food waste level!'),
-    token=registration_token,
-)
+def dailyNotif():
+    msg = "Time to check today's food waste level!"
+    message = messaging.Message(
+        notification=_messaging_utils.Notification('Crumbster Alert', msg),
+        token=registration_token,
+    )
 
+    response = messaging.send(message)
+    print('Successfully sent message (daily):', msg, response)
+
+
+def overNotif(n):
+    msg = "You've added "+n+"g of waste! Please be conscious of your food waste levels :)"
+    message = messaging.Message(
+        notification=_messaging_utils.Notification('Crumbster Alert', msg),
+        token=registration_token,
+    )
+
+    response = messaging.send(message)
+    print('Successfully sent message (over):', msg, response)
+
+
+def maxNotif():
+    msg = "You've reached the maximum weight capacity of your waste bin!"
+    message = messaging.Message(
+        notification=_messaging_utils.Notification('Crumbster Alert', msg),
+        token=registration_token,
+    )
+
+    response = messaging.send(message)
+    print('Successfully sent message (max):', msg, response)
