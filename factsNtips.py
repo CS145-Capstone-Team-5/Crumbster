e338397fa2c3f584e570b0a7e656bc328bbb7a64
sumulongnj <njsumulong@up.edu.ph>
2023-06-03 12:57:22 +0800

    Added more facts and tips

--- factsNtips.py
+++ factsNtips.py
@@ -3,11 +3,35 @@
 facts = {
     'fact1': {'fact': "The scale of the current global hunger and malnutrition crisis is enormous, with more than 345 million people facing high levels of food insecurity in 2023 - more than double the number in 2020. - WFP"},
     'fact2': {'fact': "More than 900,000 people worldwide are fighting to survive in catastrophic hunger/one step away from famine. This is ten times more than five years ago, an alarmingly rapid increase. - WFP"},
-    'fact3': {'fact': "The climate crisis is one of the leading causes of the steep rise in global hunger. Climate shocks destroy lives, crops and livelihoods, and undermine people's ability to feed themselves. - WFP"}
+    'fact3': {'fact': "The climate crisis is one of the leading causes of the steep rise in global hunger. Climate shocks destroy lives, crops and livelihoods, and undermine people's ability to feed themselves. - WFP"},
+    'fact4': {'fact': "1,717 metric tons of food are wasted every day in the Philippines. - DOST-FNRI"},
+    'fact5': {'fact': "Every Filipino wastes one tablespoon or 14 grams of rice every day, accounting for ₱23 million a day worth of wasted rice a day. - WWF Philppines"},
+    'fact6': {'fact': "Rice, vegetables and meat are the top three most wasted food. - DOST-FNRI"},
+    'fact7': {'fact': "The Philippines was ranked 69 of 119 countries with a serious level of hunger incidence by the Global Hunger Index of 2018. - GHI 2018"},
+    'fact8': {'fact': "About 11.8 percent of Filipino families, or an estimated three million households, experienced hunger in the fourth quarter of 2022. - SWS Survey 2023"},
+    'fact9': {'fact': "Larger meal portion size, a greater number of household members, and higher wealth status lead to food waste. - DOST-FNRI"},
+    'fact10': {'fact': "There is more food wasted in households with five or more members; smaller households are more efficient in consuming food. - DOST-FNRI"},
+    'fact11': {'fact': "Rice wastage is more common in households with a head of 50 to 69 year old compared to those with a younger household head. - DOST-FNRI"},
+    'fact12': {'fact': "The Zero Food Waste Act of 2022 is aiming to adopt a system that promotes food waste reduction through redistribution and recycling. - Senate Bill 240"},
+    'fact13': {'fact': "Millions of Filipinos under poverty and experiencing food insecurity are struggling to be fed, and the food that is simply thrown away or discarded might actually be enough to feed them. - Dr. Imelda Angeles Agdeppa"},
+    'fact14': {'fact': "The top three food wasted (rice, vegetables and meat) can become real meals for the hungry. - DOST-FNRI"},
+    'fact15': {'fact': "3.4 million families in the Philippines have gone hungry “at least once” between April to June 2021, still above the pre-COVID crisis rate, which is 8.8 percent or 2.1 million families. - Social Weather Stations"}
 }
 
 tips = {
-    'tip4': {'tip': "Preplan and write your shopping list before going to the grocery store. As you write your list, think about what meals you will be preparing the following week, and check your fridge to see what items you already have. - FDA"},
-    'tip5': {'tip': "When at the store, buy only what you need and stick to your shopping list. Be careful when buying in bulk, especially with items that have a limited shelf life. - FDA"},
-    'tip6': {'tip': "Prepared too much food for a party at your home? Pack extras in containers for guests to take home or take some over to a neighbor as a nice gesture. - FDA"}
+    'tip1': {'tip': "Preplan and write your shopping list before going to the grocery store. As you write your list, think about what meals you will be preparing the following week, and check your fridge to see what items you already have. - FDA"},
+    'tip2': {'tip': "When at the store, buy only what you need and stick to your shopping list. Be careful when buying in bulk, especially with items that have a limited shelf life. - FDA"},
+    'tip3': {'tip': "Prepared too much food for a party at your home? Pack extras in containers for guests to take home or take some over to a neighbor as a nice gesture. - FDA"},
+    'tip4': {'tip': "Buy “ugly produce” or oddly-shaped produce, which are just as nutritious; those will end up in the thrash if not purchased. - WWF Philippines"},
+    'tip5': {'tip': "Plan your meals and create a shopping list to avoid buying what is not needed that will be thrown away. Practice FIFO (first in, first out) to avoid food spoilage. - WWF Philippines"},
+    'tip6': {'tip': "Know your family’s serving size to reduce what you throw away after a meal. - WWF Philippines"},
+    'tip7': {'tip': "Cook leftovers, transforming them into another dish. - WWF Philippines"},
+    'tip8': {'tip': "Share excess food; avoid stashing them in the ref only to be thrown days later after it becomes spoiled food. - WWF Philippines"},
+    'tip9': {'tip': "Today, do your share in reducing food waste. Check what you have at home before buying new food stuff. - DOST-FNRI"},
+    'tip10': {'tip': "Even if a product is not attractive, give it a chance. It is still healthy and tasty. - WWF"},
+    'tip11': {'tip': "Before going to the market, list things you need and stick to your it, you will save money while saving food from being wasted. - WWF"},
+    'tip12': {'tip': "Plan meals to determine the things to buy. - WWF"},
+    'tip13': {'tip': "Avoid spoilage by always checking food storage. - WWF"},
+    'tip14': {'tip': "Cook and serve only the size of meals your household will be able to eat. - WWF"},
+    'tip15': {'tip': "Learn to be creative and never disregard leftovers. - WWF"}
 }
