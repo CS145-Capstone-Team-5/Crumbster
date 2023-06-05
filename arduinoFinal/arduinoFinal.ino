2d0e3b4b4480d76f10879ee88cde10cfe6cf58e6
Jeanne Toledo <jeanneclarissetoledo@gmail.com>
2023-06-05 14:50:30 +0800

    Configured IP and variable in Arduino code

--- arduinoFinal/arduinoFinal.ino
+++ arduinoFinal/arduinoFinal.ino
@@ -18,7 +18,7 @@ IPAddress DNSserver(8,8,8,8);
 IPAddress subnet(255,255,255,0);
 
 //Server Config
-IPAddress server(192,168,254,102);
+IPAddress server(192,168,254,100);
 
 // HX711 circuit wiring
 const int LOADCELL_DOUT_PIN = A4;
@@ -36,6 +36,7 @@ void setup() {
 
   scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
   scale.set_scale(-409.42);
+
   scale.tare();
   
 }
@@ -43,8 +44,13 @@ void setup() {
 void loop() {
   weight = scale.get_units(5);
   Serial.print("Weight:\t");
-  Serial.print(weight, 0);
+  //Serial.print(weight, 0);
+  Serial.print(weight);
   Serial.println(" g");
+
+  int finalWeight = weight;
+
+  Serial.println(finalWeight);
 
   int contentLength = 8 + String(weight,0).length();
   //Serial.println(String(8 + String(weight,0).length()));
@@ -54,13 +60,13 @@ void loop() {
     //Serial.println(String(String(weight,0).length()));
     
     client.println("PUT /log HTTP/1.1");
-    client.println("Host: 192.168.254.102:5000");
+    client.println("Host: 192.168.254.100:5000");
     client.println("Content-Type: application/x-www-form-urlencoded");
     //Serial.println("Content-Length: " + String(contentLength));
     client.println("Content-Length: " + String(contentLength));
     client.println();
     client.print("dataLog=");
-    client.println(weight);
+    client.println(finalWeight);
   }
   
   delay(2000);
